64353f15f3f17c2c8694b1c54b627d862628b0ec
Victor Mafra <victorvmafra97@gmail.com>
2023-05-11 18:47:26 -0300

    another adjusment avaiable games rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -3,13 +3,16 @@ import { db } from "../database/database.connection.js"
 
 export async function postRentals(req, res){
     const { customerId, gameId, daysRented } = req.body
+    const { id } = req.params
 
     const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId])
     const game = await db.query(`SELECT * FROM games WHERE id=$1;`, [gameId])
-
+    const rentedGames = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id])
     console.log(game.rows[0].stockTotal)
 
-    if (customer.rows[0].length === 0 || game.rows[0].length === 0 || game.rows[0].stockTotal < 1) return res.sendStatus(400)
+    const avaiableGames = game.rows[0].stockTotal - rentedGames.rows.length
+
+    if (customer.rows[0].length === 0 || game.rows[0].length === 0 || avaiableGames > 0) return res.sendStatus(400)
     const rentDate = dayjs().format('YYYY/MM/DD')
     const originalPrice = game.rows[0].pricePerDay * daysRented
     
