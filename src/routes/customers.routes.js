782a123fbb46eed771663c740e20cb9fb2d66441
Victor Mafra <victorvmafra97@gmail.com>
2023-05-10 21:09:39 -0300

    adjust schema customers

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { getCustomers, getCustomersID, postCustomer, putCustomer } from "../controllers/customers.controllers.js"
+import { validateSchemas } from "../middlewares/validateSchema.middlewares.js"
+import { customersSchema } from "../schemas/customers.schemas.js"
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", getCustomers)
 customersRouter.get("/customers/:id", getCustomersID)
-customersRouter.post("/customers", postCustomer)
+customersRouter.post("/customers",validateSchemas(customersSchema) ,postCustomer)
 customersRouter.put("/customers/:id",putCustomer)
 
 export default customersRouter
