83307320d6c0ddfc439cca9d5aaafc5a90dd916b
Victor Mafra <victorvmafra97@gmail.com>
2023-05-12 16:46:15 -0300

    adjustment date birthdays format

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -4,6 +4,15 @@ import { db } from "../database/database.connection.js"
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT * FROM customers;`)
+
+        console.log(customers.rows.length)
+
+        for (let i = 0; i < customers.rows.length; i++) {
+            const date = new Date(customers.rows[i].birthday);
+            const formattedDate = date.toISOString().substring(0, 10);
+            customers.rows[i].birthday = formattedDate;
+          }
+
         res.send(customers.rows).status(200)
     } catch (err) {
         res.status(500).send(err.message)
@@ -14,6 +23,13 @@ export async function getCustomersID(req, res) {
     const { id } = req.params
     const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
     if (customer.rows.length === 0) return res.sendStatus(404)
+
+    
+    const date = new Date(customer.rows[0].birthday);
+    const isoDate = date.toISOString().slice(0, 10);
+
+    customer.rows[0].birthday = isoDate;
+    console.log(customer.rows[0].birthday)
 
     try {
         return res.send(customer.rows[0])
@@ -30,6 +46,7 @@ export async function postCustomer(req, res) {
     if (customerExist.rows.length !== 0) return res.sendStatus(409)
 
     const adjustedBirthday = dayjs(birthday).format('YYYY/MM/DD')
+    console.log(adjustedBirthday)
 
     try {
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) 
