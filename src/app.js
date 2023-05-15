33f947872eb5cb56950275859441169f084b194c
Victor Mafra <victorvmafra97@gmail.com>
2023-05-15 12:02:26 -0300

    hore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.routes.js"
+import dotenv from "dotenv"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+dotenv.config()
 
-const PORT = 5000
-app.listen(PORT, () => console.log('Using port 5000'))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log('Using port 5000'))
